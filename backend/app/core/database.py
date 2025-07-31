0262ec7d70476ef453b3c1205022b80286d7029c
Miles Shinmachi <78616843+miles-akio@users.noreply.github.com>
2025-07-30 23:59:04 -0700

    Create database.py

---
+++ backend/app/core/database.py
@@ -0,0 +1,18 @@
+# ===== BACKEND/APP/CORE/DATABASE.PY =====
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+metadata = MetaData()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
